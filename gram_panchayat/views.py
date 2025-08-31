fea629dad9cf71cd223bbe05b501fa709042219a
Sandip-2006 <sandip97316@gmail.com>
2025-08-31 11:32:31 +0530

    Resolved merge conflict in views.py (kept both complain and contact views)

--- gram_panchayat/views.py
+++ gram_panchayat/views.py
@@ -3,12 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request,"index.html")
+    return render(request, "index.html")
 
-<<<<<<< HEAD
 def complain(request):
-    return render(request,"complain.html")
-=======
+    return render(request, "complain.html")
+
 def contact(request):
-    return render(request,"contact.html")
->>>>>>> 8c99a8b01db18f604c74ea6a6a0389a1742ace03
+    return render(request, "contact.html")
