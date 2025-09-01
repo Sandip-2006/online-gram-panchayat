13ab9e3efcaaa57206ce3e20e585832b6be08074
Sandip-2006 <sandip97316@gmail.com>
2025-09-01 16:05:43 +0530

    complain section responcive

--- project/settings.py
+++ project/settings.py
@@ -45,13 +45,10 @@ INSTALLED_APPS = [
 ]
 
 
-TAILWIND_APP_NAME = 'theme'
-INTERNAL_IPS = ['127.0.0.1']
-
-# npm 
-NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
-
-
+TAILWIND_APP_NAME='theme'
+INTERNAL_IPS=['127.0.0.1']
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
+ 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
