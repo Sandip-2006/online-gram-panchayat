f3cdab52afcf6df85019683bab0d32610f18a35e
Sandip-2006 <sandip97316@gmail.com>
2025-09-07 17:00:45 +0530

    Completed backend implementation for contact, event, celebrity, and celebrity detail pages

--- gram_panchayat/forms.py
+++ gram_panchayat/forms.py
@@ -1,8 +1,12 @@
 from django import forms
-from .models import complain
+from .models import complain,Contact
 
 class complain_form(forms.ModelForm):
     class Meta:
         model = complain
         fields = ['fname', 'phone_num', 'Email', 'complain', 'Complaint_Details', 'photo']
 
+class contact_form(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['name','email','subject','message']
