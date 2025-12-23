bd9d182036575b5b89ef002e1c23e3abf03f5478
Sandip-2006 <sandip97316@gmail.com>
2025-12-23 17:42:38 +0530

    add postgres database setup and producation ready configurations

--- project/settings.py
+++ project/settings.py
@@ -12,21 +12,27 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(dotenv_path=BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+# TODO: don't write env everywhere, create an object of env and then use it.(Done)
+env = os.environ.get
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t@)(%j0fkr=g@!%=9gt&z1v1w3zpqxfgd^5fgcf4!qf*3#^k4v'
+SECRET_KEY = env("SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+# TODO: This should be picked from environment variables (.env).(Done)
+ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
@@ -103,6 +109,18 @@ DATABASES = {
 #         'PORT': '3306',                        # Default MySQL port
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),  # The database you created
+        "USER": env("DB_USER"),  # PostgreSQL user
+        "PASSWORD": env("DB_PASSWORD"),  # Password you set
+        "HOST": env("DB_HOST"),  # Or an IP if remote
+        "PORT": env("DB_PORT"),  # Default PostgreSQL port
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
