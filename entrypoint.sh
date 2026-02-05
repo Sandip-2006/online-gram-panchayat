039f0838561b737442ef66b83b31c7d8d2b2bd26
Sandip-2006 <sandip97316@gmail.com>
2026-02-05 20:47:52 +0530

    update entrypoint.sh and django-cd file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
+set -e
+
+echo "⏳ Waiting for startup..."
+sleep 5
 
 echo "📦 Running migrations..."
 python manage.py migrate --noinput
+
+echo "🧹 Clearing old static files..."
+rm -rf /app/staticfiles/*
 
 echo "📁 Collecting static..."
 python manage.py collectstatic --noinput
 
 echo "🚀 Starting Gunicorn..."
-exec gunicorn project.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn project.wsgi:application \
+  --bind 0.0.0.0:8000 \
+  --workers 3 \
+  --timeout 120
